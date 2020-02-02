e2dd3260203330979664a020bf3cd3b076f552c3
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-02-02 16:40:07 +0000

    adds repayment calculator

--- src/components/CalculatedResults.js
+++ src/components/CalculatedResults.js
@@ -1,12 +1,25 @@
 import React from 'react';
 
-const ResultsDisplay = (props) => {
+const CalculatedResults = (props) => {
+
+  // const getMonthlyPayments = () => {
+  //   const annual = props.loanAmount / props.repaymentTime;
+  //   const interest = (annual / 100) * props.interestRate;
+  //   const monthly = (annual / 12) + interest;
+  //   return Math.ceil(monthly);
+  // }
 
   const getMonthlyPayments = () => {
-    const annual = props.loanAmount / props.repaymentTime;
-    const interest = (annual / 100) * props.interestRate;
-    const monthly = (annual / 12) + interest;
-    return Math.ceil(monthly);
+    const total = props.loanAmount;
+    const monthly_interest = (props.interestRate / 100) / 12;
+    const number_of_payments = props.repaymentTime * 12;
+
+    const numerator = (monthly_interest * Math.pow(1 + monthly_interest, number_of_payments));
+    const denominator = Math.pow(1 + monthly_interest, number_of_payments) - 1;
+
+    const monthly_payments = total * (numerator / denominator);
+
+    return parseInt(monthly_payments);
   }
 
   const getTotalAmountRepayable = () => {
@@ -25,4 +38,4 @@ const ResultsDisplay = (props) => {
 
 }
 
-export default ResultsDisplay;
+export default CalculatedResults;
